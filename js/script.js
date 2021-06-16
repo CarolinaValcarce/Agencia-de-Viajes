4dfa270cd0d88befee9a1e355feeafd95bfc9498
CarolinaValcarce <carolina.valarce@hotmail.com>
2021-06-16 23:12:18 +0200

    First commit

---
+++ js/script.js
@@ -0,0 +1,18 @@
+
+/*=============================================
+PINTEREST GRID
+=============================================*/
+
+$('.pinterest_grid').pinterest_grid({
+	no_columns: 4, //Número de columnas
+	padding_x: 10, //Márgenes internas horizontal
+	padding_y: 10, //Márgenes internas vertical
+	margin_bottom: 50, //Márgen externa inferor
+	single_column_breakpoint: 769 //Punto de quiebre para una sola columna
+});
+
+/*=============================================
+STICKY JS
+=============================================*/
+
+// $("#top").sticky({topSpacing:0, zIndex:1000});
